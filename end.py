246573193cbdd4746d6b701aff67b6b8fc8b1916
reda <redaermich@gmail.com>
2022-08-09 13:57:51 +0100

    first challenge

---
+++ end.py
@@ -0,0 +1,17 @@
+# day one
+# first challenge
+name_one = "ERMIX"
+name_two = "ERMIX_RE"
+
+# Needed Output => Length of the string is 20
+# @@@@@@@@@@@@@@@ERMIX
+# @@@@@@@@@@@@ERMIX_RE
+
+
+# 1rst method
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
+
+
+
+
---
+++ start.py
@@ -0,0 +1,9 @@
+# day one
+# first challenge
+name_one = "ERMIX"
+name_two = "ERMIX_RE"
+
+# Needed Output => Length of the string is 20
+# @@@@@@@@@@@@@@@ERMIX
+# @@@@@@@@@@@@ERMIX_RE
+
