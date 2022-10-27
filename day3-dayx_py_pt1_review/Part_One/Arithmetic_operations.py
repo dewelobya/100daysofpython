90696ce0b45e8ef90a9000cf6a6b66913e532e11
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2022-10-27 12:25:22 +0300

    changes

---
+++ day3-dayx_py_pt1_review/Part_One/Arithmetic_operations.py
@@ -0,0 +1,16 @@
+#Arithmetic operations
+#You will be able to use variables in various arithmetic operations
+#You will know how to deal with numbers in user input
+#You will know how to cast values into other fundamental data types
+#example 
+
+height = 172.5
+weight = 68.55
+
+# the Body Mass Index, or BMI, is calculated by dividing body mass with the square of height
+# height is converted into metres in the formula
+bmi = weight / (height / 100) ** 2
+
+print(f"The BMI is {bmi}")
+
+#The BMI is 23.037177063642087
