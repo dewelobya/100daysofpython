72d375c9c382d90bde310ab74f9b5d269185d8e9
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2023-01-15 14:54:15 +0200

    cleaned
    cleaned to another space

---
+++ day3 /AutomateWhatsappMessages.py
@@ -0,0 +1,15 @@
+import pywhatkit as pwk
+
+pwk.sendwhatmsg("+358451330622", 'Eikö ollut muita puuhia?', 20, 52)
+
+
+#import pywhatkit as pwk
+#pwk.sendwhatmsg_instantly("+91XXXXXXXXXX", 'Hello!')
+
+#problem is that it opens the web version of the whatsapp but doesn't send the message to the number,
+#I'll have to push the enter to send.
+
+
+#import pywhatkit as pwk
+
+# pwk.sendwhatmsg_to_group_instantly("H3JukSmlGGUJzPZPkiw4oB", 'Hello!')
--- day3-dayx_py_pt1_review/Part_One/CalcSeconds.py
+++
@@ -1,6 +0,0 @@
-#calculate how many seconds in those days
-
-# Write your solution here 
-day = int(input("How many days?"))
-seconds = day*24*60*60
-print(f"Seconds in that many days: {seconds}")
